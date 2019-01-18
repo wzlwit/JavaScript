1e8ee2f9d47a78ae90802ec9cbd002a6789e92df
Zhaolong Wang <zl.wangadfa@hotmail.com>
2019-01-18 16:37:44 -0500

    Stateless Component is done

--- React/Workshop/LearnReactJS_CodeCademy/PartII/1 StatelessComponentsFromStatefullComponents.js
+++ React/Workshop/LearnReactJS_CodeCademy/PartII/1 StatelessComponentsFromStatefullComponents.js
@@ -87,9 +87,7 @@ ReactDOM.render(
 
 
 /* Child Components Update Sibling Components -- PATTERN 
-
  A child component updates its parent's state, and the parent passes that state to a sibling component.
-
 */
 
 //Parent.js
@@ -179,3 +177,96 @@ export class Sibling extends React.Component {
 
 
 /* Video Player */
+//App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Video } from './Video';
+import { Menu } from './Menu';
+
+const VIDEOS = {
+    fast: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4',
+    slow: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4',
+    cute: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4',
+    eek: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4'
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { src: VIDEOS.fast };
+        this.chooseVideo = this.chooseVideo.bind(this);
+    }
+    chooseVideo(newVideo) {
+        this.setState({ src: VIDEOS[newVideo] });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Video Player</h1>
+                <Menu chooseVideo={this.chooseVideo} />
+                <Video src={this.state.src} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+
+//Menu.js
+import React from 'react';
+
+export class Menu extends React.Component {
+    render() {
+        return (
+            <form onClick={this.props.chooseVideo}>
+                <input type="radio" name="src" value="fast" /> fast
+                <input type="radio" name="src" value="slow" /> slow
+                <input type="radio" name="src" value="cute" /> cute
+                <input type="radio" name="src" value="eek" /> eek
+            </form>
+        );
+    }
+}
+
+//Video.js
+import React from 'react';
+
+export class Video extends React.Component {
+    render() {
+        return (
+            <div>
+                <video controls autostart autoplay src={this.props.src} />
+            </div>
+        );
+    }
+}
+
+//Menu.js
+import React from 'react';
+
+export class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        const text = e.target.value;
+        this.props.chooseVideo(text);
+    }
+    render() {
+        return (
+            <form onClick={this.handleClick}>
+                <input type="radio" name="src" value="fast" /> fast
+                <input type="radio" name="src" value="slow" /> slow
+                <input type="radio" name="src" value="cute" /> cute
+                <input type="radio" name="src" value="eek" /> eek
+            </form>
+        );
+    }
+}

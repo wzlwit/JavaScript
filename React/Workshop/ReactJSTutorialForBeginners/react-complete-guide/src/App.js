d87cdb01c92b7621dd93f218079b7cb922e926f6
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-01-21 11:13:57 -0500

    short class for review React by a simple page

--- React/Workshop/ReactJSTutorialForBeginners/react-complete-guide/src/App.js
+++ React/Workshop/ReactJSTutorialForBeginners/react-complete-guide/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Person/Person.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  /*   constructor(props){
+      super(props);
+      this.switchNameHandler=this.switchNameHandler.bind(this);
+    } */
 
   state = {
     persons: [
@@ -11,34 +16,57 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'other information'
+    otherState: 'some other value'
   }
-  switchNameHandler() {
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked');
     //DON'T DO THIS: this.state.persons[0].name="Maximilian";
-    this.setState(
-      {
-        persons: [
-          { name: 'Max', age: 28 },
-          { name: 'Manu', age: 29 },
-          { name: 'Stephanie', age: 27 }
-        ]
-      }
-    )
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
-
+  nameChangedHandler = (e) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: e.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: 8
+    };
     return (
+
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/*  <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button> // .bind() like .call()*/}
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Maxillian')}
+        >Switch Name</button>
+        {/*pass an inline anonymous function;
+        this way is easy to pass an argument*/}
+
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>My hobbies: racing!</Person>
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}
+        >My hobbies: Racing!</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
--- React/Workshop/ReactJSTutorialForBeginners/react-complete-guide/src/Person/Person.js
+++ React/Workshop/ReactJSTutorialForBeginners/react-complete-guide/src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const person = (props) => {//only rendering
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>{/* access its children for class, otherwise it will not show */}
+            <input type='text' onChange={props.changed} defaultValue={props.name/* here, using 'value' will give an error, which make the input read-only */}></input>
         </div>
     );
 }

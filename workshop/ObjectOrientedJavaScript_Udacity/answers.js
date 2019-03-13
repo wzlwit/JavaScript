d5a48b952db21637883942dee9f42cc7b5b6e12a
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-03-13 16:57:21 -0400

    lesson 3 fini

--- workshop/ObjectOrientedJavaScript_Udacity/answers.js
+++ workshop/ObjectOrientedJavaScript_Udacity/answers.js
@@ -287,3 +287,37 @@ const myFunction = (
         }
     }
 )();
+
+
+/**
+ * *Lesson 3: Classes and Objects
+ * 2. Constructor Functions
+
+Now it's your turn to create a constructor function. Declare a
+`Sandwich` constructor function that takes three parameters:
+
+1. `bread` (string) - the type of bread for the sandwich (e.g. "Wheat")
+2. `meat` (array) - the meats to put on the sandwich
+   (e.g. `[]` for a vegetarian sandwich!)
+3. `vegetables` (array) - the vegetables to include in the sandwich
+
+*/
+const Sandwich = function (bread, meat, vegetables) {
+    this.bread = bread;
+    this.meat = meat;
+    this.vegetables = vegetables;
+}
+
+// 4. Setting Our Own 'this'
+const driver = {
+    name: 'Danica',
+    displayName: function () {
+        console.log(`Name: ${this.name}`);
+    }
+};
+
+const car = {
+    name: 'Fusion'
+};
+
+driver.displayName.bind(car);

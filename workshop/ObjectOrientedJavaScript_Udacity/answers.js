1763b42d383dfacc258779d481c35377c15c2622
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-03-13 15:22:32 -0400

    lesson 2 fini

--- workshop/ObjectOrientedJavaScript_Udacity/answers.js
+++ workshop/ObjectOrientedJavaScript_Udacity/answers.js
@@ -119,12 +119,171 @@ const chameleon = {
  * Write an expression using Object.keys() to extract the keys (i.e., property names) from the triangle object:
  */
 const triangle = {
-  type: 'polygon',
-  sides: 3,
-  sumOfAngles: 180,
-  equilateral: true,
-  equiangular: true
+    type: 'polygon',
+    sides: 3,
+    sumOfAngles: 180,
+    equilateral: true,
+    equiangular: true
 };
 
 Object.keys(triangle);
 
+/**
+ * *Lesson 2: Functions at Runtime
+ * 2. First-Class Functions
+ */
+
+/**
+ * todo: Question
+Declare a function named `higherOrderFunction` that takes no arguments,
+and returns an anonymous function.
+
+The returned function itself takes no arguments as well, and simply
+returns the number 8.
+*/
+
+const higherOrderFunction = () => {
+    return () => {
+        return 8;
+    }
+}
+
+/**
+ * 3. Callbacks
+ * todo: Question 4 of 5
+ * * Using map()
+ *
+ * Using the musicData array and map():
+ *   - Return a string for each item in the array in the following format:
+ *     <album-name> by <artist> sold <sales> copies
+ *   - Store the returned data in a new albumSalesStrings variable
+ *
+ * Note:
+ *   - Do not delete the musicData variable
+ *   - Do not alter any of the musicData content
+ *   - Do not format the sales number; leave it as a long string of digits
+ */
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    {
+        artist: 'Original Broadway Cast Recording',
+        name: 'Hamilton: An American Musical', sales: 820000
+    },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const albumSalesStrings = musicData.map(value => {
+    return `${value.name} by ${value.artist} sold ${value.sales} copies`;
+});
+
+console.log(albumSalesStrings);
+
+/**
+ * todo: Question 5 of 5
+ * * Using filter()
+ *
+ * Using the musicData array and filter():
+ *   - Return only album objects where the album's name is
+ *     10 characters long, 25 characters long, or anywhere in between
+ *   - Store the returned data in a new `results` variable
+ *
+ * Note:
+ *   - Do not delete the musicData variable
+ *   - Do not alter any of the musicData content
+ */
+
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    {
+        artist: 'Original Broadway Cast Recording',
+        name: 'Hamilton: An American Musical', sales: 820000
+    },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const results = musicData.filter(ele => {
+    return ele.name.length >= 10 && ele.name.length <= 25;
+});
+
+console.log(results);
+
+
+// * 5. Closures
+// https://classroom.udacity.com/courses/ud711/lessons/fd2e70b8-84b4-4793-a89e-78a695ecf89d/concepts/4a704701-a6ec-4882-8075-13524d1cd68a
+
+
+function outerFunction() {
+    let num1 = 5;
+
+    return function (num2) {
+        console.log(num1 + num2);
+    };
+}
+
+let result = outerFunction();//result refers to inner function, the closure
+
+result(10);
+//15
+
+/**
+ * todo: Applications of Closures
+Declare a function named `expandArray()` that:
+
+* Takes no arguments
+* Contains a single local variable, `myArray`, which points to [1, 1, 1]
+* Returns an anonymous function that directly modifies `myArray` by
+appending another `1` into it
+* The returned function then returns the value of `myArray`
+
+*/
+function expandArray() {
+    var myArray = [1, 1, 1];
+    return () => {
+        return [...myArray, 1];
+    }
+}
+
+function expandArray() {
+    var myArray = [1, 1, 1];
+    return function () {
+        myArray.push(1);
+        return myArray;
+    };
+}
+
+/**
+ * 6. Immediately-invoked Function Expression
+ */
+(function (x, y) {
+    console.log(x * y);
+}
+)(2, 3);
+
+(function sayHi() {
+    alert('Hi there!');
+}());
+
+
+//  todo: private scope
+const myFunction = (
+    function () {
+        const hi = 'Hi!';
+        return function () {
+            console.log(hi);
+        }
+    }
+)();
